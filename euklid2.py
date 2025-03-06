30956b4e59ff538976bca3f15d2797b7837be413
Mark Baumann <mark.baumann@cib.de>
2025-03-06 15:35:11 +0100

    add euklid2

--- euklid2.py
+++ euklid2.py
@@ -1,15 +1,13 @@
-def euklidischer_algorithmus_1(a, b):
+def euklidischer_algorithmus_2(a, b):
     original_a, original_b = a, b
-    while a != b:
-        if a > b:
-            a-=b
-        else:
-            b-=a
+
+    while b!=0:
+        a, b = b, a%b
     return a, original_a, original_b
 
 a = int(input("Gib eine natürliche Zahl a ein: "))
 b = int(input("Gib eine natürliche Zahl b ein: "))
 
-ggt, zahl1, zahl2 = euklidischer_algorithmus_1(a, b)
+ggt, zahl1, zahl2 = euklidischer_algorithmus_2(a, b)
 
 print(f"Der größte gemeinsame Teiler von {zahl1} und {zahl2} ist {ggt}.")
