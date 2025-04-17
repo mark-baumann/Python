c940fffc055ecd79052952557afbedc7717284bb
Mark Baumann <kontakt@markb.de>
2025-04-17 18:24:18 +0200

    add lambda and listComprehension

--- Basics/functionalProg.py
+++ Basics/functionalProg.py
@@ -7,4 +7,7 @@ numbers = [x for x in range(10)]
 squared = list(map(lambda x: x*2, numbers))
 print(squared)
 
-
+#filter
+numbers = [1, 2, 3, 4, 5]
+even = list(filter(lambda x: x % 2 == 0, numbers))
+print(even)  # [2, 4]
